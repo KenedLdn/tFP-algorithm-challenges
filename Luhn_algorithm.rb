55ca0dbc3f023603ae0322ed69dda6a5a41f63dd
Ken Ho <ken.ho@hotmail.co.uk>
2015-12-20 03:10:08 +0000

    Implement Luhn algorithm verified with four test cases

---
+++ Luhn_algorithm.rb
@@ -0,0 +1,37 @@
+require 'minitest/autorun'
+
+module Luhn
+  def self.is_valid?(number)
+    arr = number.to_s.split(//).map {|str| str.to_i}
+    n = 1
+    while n < arr.size
+      arr.size.even? ? arr[n - 1] *= 2 : arr[n] *= 2
+      n += 2
+    end
+    puts arr.inspect
+    sum_digits = arr.collect {|digit| digit >= 10 ? (digit - 9) : digit}
+    puts sum_digits.inspect
+    puts sum_digits.reduce(:+).inspect
+    sum_digits.reduce(:+) % 10 == 0
+  end
+end
+
+class TestLuhn < MiniTest::Unit::TestCase
+
+  def test_luhn_valid
+    assert Luhn.is_valid?(4194560385008504)
+  end
+
+  def test_luhn_invalid
+    assert ! Luhn.is_valid?(4194560385008505)
+  end
+
+  def test_luhn_valid2
+    assert Luhn.is_valid?(377681478627336), "Check step two: Did you start at the right?"
+  end
+
+  def test_luhn_invalid2
+    assert ! Luhn.is_valid?(377681478627337), "Check step two: Did you start at the right?"
+  end
+
+end
